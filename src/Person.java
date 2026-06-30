a2308d5367bdc7d5c8ab30f49411428c602329c9
Durant <gunnar.durant@protonmail.com>
2026-06-30 06:04:13 -0700

    module 6

--- src/Person.java
+++ src/Person.java
@@ -11,41 +11,12 @@ public class Person {
         this.lastName = lastName;
         this.dateOfBirth = dateOfBirth;
     }
-    /**
-     * Returns a String of the firstName
-     * @return firstName
-     */
-    public String getFirstName(){
-        return firstName;
-    }
-    /**
-     * Returns a string of the middleName
-     * @return middleName
-     */
-    public String getMiddleName(){
-        return middleName;
-    }
-    /**
-     * Returns a String of the lastName
-     * @return lastName
-     */
-    public String getLastName(){
-        return lastName;
-    }
-    /**
-     * Returns a concatenated string of the Person's name
-     * @return the Person's first, middle, and last name.
-     */
-    public String getName(){
-        return firstName + " " + middleName + " " + lastName;
-    }
-    /**
-     * Returns the Person's date of birth as a date type
-     * @return a Date type of the Person's date of birth.
-     */
-    public Date getDateOfBirth(){
-        return dateOfBirth;
-    }
+   //all getters
+    public String getFirstName(){ return firstName; }
+    public String getMiddleName(){ return middleName; }
+    public String getLastName(){ return lastName;}
+    public String getName(){ return firstName + " " + middleName + " " + lastName; }
+    public Date getDateOfBirth(){ return dateOfBirth; }
 
 
 }
