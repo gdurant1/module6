b89ea510f24d2cfceeea621aa7a4670c682a5c7a
Durant <gunnar.durant@protonmail.com>
2026-06-30 08:58:29 -0700

    module 6

--- src/Student.java
+++ src/Student.java
@@ -2,12 +2,30 @@ import java.time.Year;
 import java.util.Date;
 
 public class Student extends Person{
-    private static double nextStudentId=0;
+    private static int nextStudentId=1;
     private int studentId;
     protected String major;
     protected String degree;
     protected double gpa;
     protected Year expectedGraduation;
+
+    public Student(){
+        this.studentId = nextStudentId;
+        this.major = "UnDeclared";
+        this.degree = "N/A";
+        this.gpa = gpa;
+        this.expectedGraduation = null;
+        nextStudentId++;
+    }
+
+    public Student(String major, String degree, double gpa, Year expectedGraduation){
+        this.studentId = nextStudentId;
+        this.major = major;
+        this.degree = degree;
+        this.gpa = gpa;
+        this.expectedGraduation = expectedGraduation;
+        nextStudentId++;
+    }
 
 
 
