080bf6512bc4eab5f89bec67f218906ab8de89b3
Durant <gunnar.durant@protonmail.com>
2026-07-01 00:32:39 -0700

    module 6

--- src/Durant_JF_7_4_5.java
+++ src/Durant_JF_7_4_5.java
@@ -1,5 +1,25 @@
+import javax.print.DocFlavor;
+
 public class Durant_JF_7_4_5 {
     public static void main(String[] args){
+        StringInstrument guitar = new StringInstrument(true, 1000, 100, 10);
+
+        System.out.println(">>>>>>>>>> Music Shop SALE <<<<<<<<<<");
+        System.out.println("Number of Strings: " + guitar.getNumStrings() );
+        System.out.println("Stock            : " + guitar.getNumInStock() );
+        System.out.println("Sale             : " + guitar.getOnSale() );
+        System.out.println("Price            : " + guitar.getPrice());
+        System.out.println("Employee Price   : " + guitar.employeeDiscount() );
+        System.out.println();
+
+        StringInstrument bass = new StringInstrument(false, 1000, 100, 10);
+
+        System.out.println(">>>>>>>>>> Music Shop <<<<<<<<<<");
+        System.out.println("Number of Strings: " + bass.getNumStrings() );
+        System.out.println("Sale             : " + bass.getOnSale() );
+        System.out.println("Price            : " + bass.getPrice() );
+        System.out.println("Employee Price   : " + bass.employeeDiscount() );
+
 
 
     }
