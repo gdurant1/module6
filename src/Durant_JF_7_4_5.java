4a6239838f8b9809b73ce8b5a04a186c214e3530
Durant <gunnar.durant@protonmail.com>
2026-07-01 00:08:20 -0700

    module 6

--- src/Durant_JF_7_4_5.java
+++ src/Durant_JF_7_4_5.java
@@ -1,2 +1,42 @@
 public class Durant_JF_7_4_5 {
+    public static void main(String[] args){
+
+
+    }
 }
+//parent class
+class Instrument {
+    private boolean onSale;
+    private double price;
+    private int numInStock;
+
+    //constructor
+    public Instrument(boolean onSale, double price, int numInStock){
+        this.onSale = onSale;
+        this.price = price;
+        this.numInStock = numInStock; }
+
+    //getters
+    public double getPrice(){
+        if(onSale){ return price*.85; } // -15%
+        return price; }
+    public boolean getOnSale(){ return onSale; }
+    public int getNumInStock(){ return numInStock; }
+
+    //setters
+    public void setOnSale(boolean onSale){ this.onSale = onSale; }
+    public void setNumInStock(int numInStock){ this.numInStock = numInStock; }
+    public void setPrice(double price){ this.price = price; }
+
+    public double employeeDiscount(){
+        return price * 0.75; } // -25%
+}
+
+class StringInstrument extends Instrument {
+    private int numStrings;
+
+    public StringInstrument(boolean onSale, double price, int numInStock, int numStrings){
+        super(onSale, price, numInStock);
+    }
+}
+
